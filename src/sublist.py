b59ca2a5b66eebd904a2a2b688faffa7f2912768
lrakai <lmrakai@gmail.com>
2020-11-13 15:30:27 -0700

    update

--- src/sublist.py
+++ src/sublist.py
@@ -1,9 +1,8 @@
 """
 This exercise stub and the test suite contain several enumerated constants.
 
-Since Python 2 does not have the enum module, the idiomatic way to write
-enumerated constants has traditionally been a NAME assigned to an arbitrary,
-but unique value. An integer is traditionally used because it’s memory
+For this challenge, enumerated constants are written as a NAME assigned to an 
+arbitrary, but unique value. An integer is traditionally used because it’s memory
 efficient.
 It is a common practice to export both constants and functions that work with
 those constants (ex. the constants in the os, subprocess and re modules).
